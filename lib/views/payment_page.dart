03c159cf724cf003045d276950aff27b8e55ad73
luckykun88 <kmisigaro@outlook.com>
2022-03-26 16:18:40 +0300

    created payment page

---
+++ lib/views/payment_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/widgets.dart';
+
+class PaymentPage extends StatefulWidget{
+  const PaymentPage({Key? key}) : super(key: key);
+
+  @override
+  PaymentPageState createState() => PaymentPageState();
+}
+
+class PaymentPageState extends State<PaymentPage>{
+    @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
